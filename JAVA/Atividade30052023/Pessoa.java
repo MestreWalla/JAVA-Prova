4a29dc65bef58757af301b11789080080fb784d9
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-05-30 17:20:35 -0300

    update

---
+++ JAVA/Atividade30052023/Pessoa.java
@@ -0,0 +1,67 @@
+package Atividade30052023;
+
+public class Pessoa {
+    String nome;
+    double altura;
+    int diaNascimento;
+    int mesNascimento;
+    int anoNascimento;
+
+    //Construtores
+    public Pessoa(String nome, double altura, int dia, int mesNascimento, int anoNascimento, int anoNascimento){
+        this.nome = nome;
+        this.altura = altura;
+        this.diaNascimento = diaNascimento;
+        this.mesNascimento = mesNascimento;
+        this.anoNascimento = anoNascimento;
+    }
+    public Pessoa() { //Construtor vazio
+    }
+   //gets and sets
+   public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
+public double getAltura() {
+    return altura;
+}
+public void setAltura(double altura) {
+    this.altura = altura;
+}
+public int getDiaNascimento() {
+    return diaNascimento;
+}
+public void setDiaNascimento(int diaNascimento) {
+    this.diaNascimento = diaNascimento;
+}
+public int getMesNascimento() {
+    return mesNascimento;
+}
+public void setMesNascimento(int mesNascimento) {
+    this.mesNascimento = mesNascimento;
+}
+public int getAnoNascimento() {
+    return anoNascimento;
+}
+public void setAnoNascimento(int anoNascimento) {
+    this.anoNascimento = anoNascimento;
+}
+//outros métodos
+//2 tipos de métodos void // return
+public void imprimir(){
+    System.out.println("Nome: "+nome);
+    System.out.println("Altura: "+altura);
+    System.out.println("Data Nascimento: "+diaNascimento+"/"+mesaNascimento+"/"+anoNascimento);
+    }
+    public int getIdade() {
+        int idade;
+        if(diaNascimento <= 30 && mesNascimento <= 5) {
+        idade = 2023 - anoNascimento;
+    } else {
+        idade = 2023 - anoNascimento -1;
+    }
+        return idade;
+    }
+}
